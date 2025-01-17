9915693d9794700e82c1a52529b7d7fe009e4afa
ikshitij <48786591+ikshitij@users.noreply.github.com>
2025-01-17 13:59:07 +0530

    Update TestSelenium.java
    imported chromedriver classes from pom.xml

--- TestSelenium.java
+++ TestSelenium.java
@@ -1,5 +1,8 @@
 package SeleniumProject.Website1testing;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
